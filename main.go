ea66973d8917f469bb0ab096e15b055e32296848
Tong Sun <suntong@cpan.org>
2021-05-24 21:14:40 -0400

    - [+] add main.go from suntong/wxsg, the 突破网页版登录限制 version

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/eatMoreApple/openwechat"
+)
+
+func main() {
+	bot := openwechat.DefaultBot(openwechat.Desktop)
+
+	// 注册消息处理函数
+	bot.MessageHandler = func(msg *openwechat.Message) {
+		if msg.IsText() {
+			fmt.Println("你收到了一条新的文本消息")
+		}
+	}
+	// 注册登陆二维码回调
+	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
+
+	// 登陆
+	if err := bot.Login(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 获取登陆的用户
+	self, err := bot.GetCurrentUser()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 获取所有的好友
+	friends, err := self.Friends()
+	fmt.Println(friends, err)
+
+	// 获取所有的群组
+	groups, err := self.Groups()
+	fmt.Println(groups, err)
+
+	// 阻塞主goroutine, 知道发生异常或者用户主动退出
+	bot.Block()
+}
