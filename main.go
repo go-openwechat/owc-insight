d4dd0c207739ae140a91270aeb9667e1932b718f
Tong Sun <suntong@cpan.org>
2021-07-25 10:57:45 -0400

    - [!] truly stop the program when too many errors

--- main.go
+++ main.go
@@ -92,8 +92,8 @@ func main() {
 			count = 1
 		}
 		// 如果发生了三次错误,那么直接退出
-		if count == 3 {
-			bot.Logout()
+		if count > 3 {
+			abortOn("Too many errors", err)
 		}
 		logIf(0, "catch-and-skip", "count", count, "err", err)
 		lastError = time.Now()
