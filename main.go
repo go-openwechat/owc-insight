3f0dbd7cf39d32834911f7a9bc3a9c3aa54db0cb
Tong Sun <suntong@cpan.org>
2021-08-22 16:08:32 -0400

    - [#] refactor messageErrorHandler() 错误处理函数 out

--- main.go
+++ main.go
@@ -89,23 +89,8 @@ func main() {
 	//bot.Caller.Client.AddHttpHook(ResponseHooker{})
 	// 注册登陆二维码回调
 	bot.UUIDCallback = ConsoleQrCode
-
-	var count int32
-	bot.GetMessageErrorHandler = func(err error) {
-		t := time.Now()
-		if t.Sub(lastError) < 30*time.Minute {
-			count++
-		} else {
-			count = 1
-		}
-		// 如果发生了三次错误,那么直接退出
-		if count > 3 {
-			abortOn("Too many errors", err)
-		}
-		logIf(0, "catch-and-skip", "count", count, "err", err)
-		lastError = time.Now()
-	}
-
+	// 注册错误处理函数
+	bot.GetMessageErrorHandler = messageErrorHandler
 	// 注册消息处理函数
 	bot.MessageHandler = textMessageHandle
 
---
+++ owc_handle_err.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+)
+
+var count int32
+
+// 错误处理函数
+func messageErrorHandler(err error) {
+	t := time.Now()
+	if t.Sub(lastError) < 30*time.Minute {
+		count++
+	} else {
+		count = 1
+	}
+	// 如果发生了三次错误,那么直接退出
+	if count > 3 {
+		abortOn("Too many errors", err)
+	}
+	logIf(0, "catch-and-skip", "count", count, "err", err)
+	lastError = time.Now()
+}
