0fa1bf88958e04a1482e44c0387c0f92895a6f4e
Tong Sun <suntong@cpan.org>
2021-08-22 16:55:37 -0400

    - [#] better format for sendUser

--- owc_handle_text.go
+++ owc_handle_text.go
@@ -12,20 +12,18 @@ func textMessageHandle(msg *openwechat.Message) {
 	lastReceived = time.Now()
 	sender, err := msg.Sender()
 	abortOn("Can't get sender", err)
+	sendUser := sender.NickName
 	// 如果是群聊消息，该方法返回的是群聊对象(需要自己将User转换为Group对象)
-	fromGroup := ""
 	if msg.IsSendByGroup() {
 		// 取出消息在群里面的发送者
-		fromGroup = fmt.Sprintf("%s", sender)
 		sender, _ = msg.SenderInGroup()
+		sendUser = fmt.Sprintf("[%s:%s]", sendUser, sender.NickName)
 		// might not be able to get sender in group, ignore error
 	}
-	fromUserName := fmt.Sprintf("%s", sender)
 
 	logIf(0, "收到消息", "type",
 		fmt.Sprintf("%d (%d,%d)", msg.MsgType, msg.AppMsgType, msg.SubMsgType),
-		"from", fromUserName, "in", fromGroup,
-		"content", "")
+		"from", sendUser, "content", "")
 	if len(msg.Content) == 0 {
 		fmt.Println(msg)
 	} else {
