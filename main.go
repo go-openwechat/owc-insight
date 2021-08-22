d491939b9ca5c34e675c47d229f4e581ee310c18
Tong Sun <suntong@cpan.org>
2021-08-22 16:00:11 -0400

    - [#] refactor textMessageHandle() out

--- main.go
+++ main.go
@@ -107,38 +107,7 @@ func main() {
 	}
 
 	// 注册消息处理函数
-	bot.MessageHandler = func(msg *openwechat.Message) {
-		lastReceived = time.Now()
-		sender, err := msg.Sender()
-		abortOn("Can't get sender", err)
-		// 如果是群聊消息，该方法返回的是群聊对象(需要自己将User转换为Group对象)
-		fromGroup := ""
-		if msg.IsSendByGroup() {
-			fromGroup = fmt.Sprintf("%s", sender)
-			sender, err = msg.SenderInGroup()
-			abortOn("Can't get sender in group", err)
-		}
-		fromUserName := fmt.Sprintf("%s", sender)
-
-		logIf(0, "收到消息", "type",
-			fmt.Sprintf("%d (%d,%d)", msg.MsgType, msg.AppMsgType, msg.SubMsgType),
-			"from", fromUserName, "in", fromGroup,
-			"content", "")
-		if len(msg.Content) == 0 {
-			fmt.Println(msg)
-		} else {
-			fmt.Println(msg.Content)
-		}
-
-		if msg.IsText() {
-			if msg.Content == "ping" {
-				msg.ReplyText("pong")
-				fmt.Println("回文本消息", msg.Content)
-			} else {
-				fmt.Println("收到文本消息", msg.Content)
-			}
-		}
-	}
+	bot.MessageHandler = textMessageHandle
 
 	// 创建热存储容器对象
 	reloadStorage := openwechat.NewJsonFileHotReloadStorage("storage.json")
---
+++ owc_handle_text.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/suntong/openwechat"
+)
+
+// 处理文本消息
+func textMessageHandle(msg *openwechat.Message) {
+	lastReceived = time.Now()
+	sender, err := msg.Sender()
+	abortOn("Can't get sender", err)
+	// 如果是群聊消息，该方法返回的是群聊对象(需要自己将User转换为Group对象)
+	fromGroup := ""
+	if msg.IsSendByGroup() {
+		fromGroup = fmt.Sprintf("%s", sender)
+		sender, err = msg.SenderInGroup()
+		abortOn("Can't get sender in group", err)
+	}
+	fromUserName := fmt.Sprintf("%s", sender)
+
+	logIf(0, "收到消息", "type",
+		fmt.Sprintf("%d (%d,%d)", msg.MsgType, msg.AppMsgType, msg.SubMsgType),
+		"from", fromUserName, "in", fromGroup,
+		"content", "")
+	if len(msg.Content) == 0 {
+		fmt.Println(msg)
+	} else {
+		fmt.Println(msg.Content)
+	}
+
+	if msg.IsText() {
+		if msg.Content == "ping" {
+			msg.ReplyText("pong")
+			fmt.Println("回文本消息", msg.Content)
+		} else {
+			fmt.Println("收到文本消息", msg.Content)
+		}
+	}
+}
